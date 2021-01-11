67d6c151da5c9948ed1875282e5678910022057c
Pere Grinus <gregjleighton@gmail.com>
2021-01-11 07:38:17 -0500

    identified problem with SQL syntax and fixed. Corrected table structure in db.

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,8 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    burger_name VARCHAR(50),
-    devoured BOOLEAN DEFAULT,
+    id INT AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR(50) NOT NULL,
+    devoured BOOLEAN NOT NULL DEFAULT 0,
+    PRIMARY KEY(id)
 );
