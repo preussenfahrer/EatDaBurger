d0ff66bd513e04910d34423ad7e89cc803ffda7d
Pere Grinus <gregjleighton@gmail.com>
2021-01-11 19:28:36 -0500

    set up connection.js to export to ORM.

--- config/connection.js
+++ config/connection.js
@@ -13,6 +13,11 @@ database: "burgers_db"
 });
 
 connection.connect(function(err) {
-    if (err) throw err;
-    start();
-})
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId)
+});
+
+module.exports = connection;
