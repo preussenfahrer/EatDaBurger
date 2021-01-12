494628d549254bb5dc070a74bac7c834f6e1cb1e
Pere Grinus <gregjleighton@gmail.com>
2021-01-11 19:43:01 -0500

    modified burger.js to require  ORM connection and connection.js from config folder. Set up export of burger.js

---
+++ models/burger.js
@@ -0,0 +1,16 @@
+// Import orm.js and burder.js
+var ormConnection = require("../config/orm.js");
+
+var connection = require("../config/connection.js");
+
+// var orm =
+
+// var connection =
+
+
+
+
+
+
+// Export file
+module.exports = burger;
