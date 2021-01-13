ce545bcb2cb6fa27414f730593eaa1ed8a9e34fc
Pere Grinus <gregjleighton@gmail.com>
2021-01-13 09:58:45 -0500

    altered schema to default input as false.

--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,7 @@ USE burgers_db;
 CREATE TABLE burgers (
     id INT AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(50) NOT NULL,
-    devoured BOOLEAN NOT NULL DEFAULT 0,
+    devoured BOOLEAN DEFAULT false,
     PRIMARY KEY(id)
 );
 
