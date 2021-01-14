abe43bbeb59b0e4c504e05f0c6a6391cd04f2fb3
Pere Grinus <gregjleighton@gmail.com>
2021-01-13 22:29:34 -0500

    added start script and jaws db env

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,21 @@
 // Set up to connect Node to mySQL
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({host: "localhost",
-port: 3306,
-user: "root",
-password: "bruceyboy143",
-database: "burgers_db"
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "bruceyboy143",
+        database: "burgers_db"
 
-});
+    });
+}
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) {
         console.error("error connecting: " + err.stack);
         return;
